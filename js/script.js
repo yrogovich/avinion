ceeafca9053672db703f256e8a40b55808933b9c
Yaroslav <yRogovich@gmail.com>
2020-01-22 17:49:57 +0300

    Savepoint 1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 $(function() {
+    // Eyes animations
+    let eyesArray = document.querySelectorAll('.photo-group .icon .eye');
+    for(let i=0; i< eyesArray.length; i++) {
+        let eyeAnimation = bodymovin.loadAnimation({
+            container: eyesArray[i],
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            speed: 2,
+            path: 'json/eye.json'
+        });
+        eyeAnimation.setSpeed(1.5);
+    }
+
     // Smile animations
     let cardWithSmile = document.querySelector('.main-card');
     let smileAnimation = bodymovin.loadAnimation({
@@ -23,13 +37,21 @@ $(function() {
         smileAnimation.goToAndPlay(7000);
     }
 
+    // Jquery LazyLoad init
     $('.lazy').Lazy();
-    
+
     //AOS
     AOS.init({
         duration: 1000, 
     });
 
+    // Smooth Scroll
+    SmoothScroll({ 
+        animationTime: 900,
+        stepSize: 85,
+    });
+
+    // Form handler
     function callHandler(form) {
         console.log('form func is running');
         let  msg = jQuery(form).serialize();
@@ -54,3 +76,24 @@ $(function() {
         });
     }
 });
+
+// Youtube
+(function() {
+	let youtube = document.querySelectorAll( ".youtube" );	
+	for (let i = 0; i < youtube.length; i++) {		
+		let source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/sddefault.jpg";		
+		let image = new Image();
+        image.src = source;
+        image.addEventListener( "load", function() {
+            youtube[ i ].appendChild( image );
+        }( i ) );		
+        youtube[i].addEventListener( "click", function() {
+            let iframe = document.createElement( "iframe" );
+                    iframe.setAttribute( "frameborder", "0" );
+                    iframe.setAttribute( "allowfullscreen", "" );
+                    iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+                    this.innerHTML = "";
+                    this.appendChild( iframe );
+        } );	
+	};	
+})();
